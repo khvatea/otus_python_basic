bc888d8e54b72427135f3f59164321c75ca9ef65
Evgeniy Khvat <evgeniy.khvat@gmail.com>
2024-11-03 21:54:33 +0300

    Project template added

---
+++ menu.py
@@ -0,0 +1,26 @@
+def open_handbook():
+    pass
+
+
+def save_handbook():
+    pass
+
+
+def show_all_handbook_row():
+    pass
+
+
+def add_raw():
+    pass
+
+
+def find_raw():
+    pass
+
+
+def update_raw():
+    pass
+
+
+def delete_raw():
+    pass
