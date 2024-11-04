5002c8d2ebc1cff1a8530e4a8cfb20dcf531e94c
Evgeniy Khvat <evgeniy.khvat@gmail.com>
2024-11-04 19:13:37 +0300

    Added functions for opening and viewing the directory

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # otus_python_basic
 Repository for the OTUS course. Python basic
+
+для вывода данных установлен пакет PrettyTable
+pip install PrettyTable
--- menu.py
+++ menu.py
@@ -1,13 +1,35 @@
-def open_handbook():
-    pass
+import json
+from dis import pretty_flags
+
+from prettytable import PrettyTable
+
+
+def open_handbook(handbook_file: str) -> list:
+    """ Открыть справочкник телефонной книги
+        :param handbook_file: JSON файл, содержащий записи контактов телефонной книги
+        :return: Объект JSON, со справочником контактов
+        """
+    with open(handbook_file, "r") as handbook:
+        return json.load(handbook)
 
 
 def save_handbook():
     pass
 
 
-def show_all_handbook_row():
-    pass
+def show_all_handbook_rows(handbook: list, sort_by_field=None) -> str:
+    pretty_table = PrettyTable()
+
+    # set table field names
+    pretty_table.field_names = ["name", "phone", "email", "address"]
+    pretty_table.align["name"] = "l"
+    pretty_table.align["address"] = "l"
+
+    # place all lines from the dictionary into a table
+    for hb in handbook:
+        pretty_table.add_row(list(hb.values()))
+
+    return pretty_table.get_string(sortby=sort_by_field)
 
 
 def add_raw():
@@ -24,3 +46,11 @@ def update_raw():
 
 def delete_raw():
     pass
+
+
+######################
+# Считываем файл и записываем в объект JSON
+hndbook = open_handbook("handbook.json")
+
+# Выводим записи справочника в таблице
+print(show_all_handbook_rows(hndbook, "name"))
